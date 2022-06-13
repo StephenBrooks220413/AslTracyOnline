39ce6862503068d4cd425546f1d496cc29be6d1d
Stephen A Brooks <46081327+StephenBrooks220413@users.noreply.github.com>
2022-06-13 06:38:56 -0500

    Update

--- controllers/Courses.js
+++ controllers/Courses.js
@@ -1,7 +1,7 @@
-const Courses = require('../models/Courses');
+const Lessons = require('../models/Courses');
 
 module.exports = async (req, res)=>{
-    const courses = await Courses.find({}).limit(40).sort({_id: -1});
+    const courses = await Lessons.find({}).limit(40).sort({_id: -1});
     res.render('courses', {
         courses
     })
